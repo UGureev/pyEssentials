23c3954297a921cfe705e692e00d4111f73351e6
UGureev <gureev.u@gmail.com>
2021-05-28 00:28:35 +0300

    add inheritance info

--- inheritance.py
+++ inheritance.py
@@ -1,25 +1,79 @@
-class A():
-    def f(self):
-        print("A")
+class MyMRO(type):  # наследование type = это метакласс
+    """задаем свой mro"""
+    def mro(cls):
+        return (cls, Animal, object)  # явно задаем порядок!
 
-class B():
-    
-    def f(self):
-        print("B")
 
-class C():
-    pass
+class Animal:
 
-class D(A):
-    pass
+    def __init__(self):
+        self.weight = 100
 
-class E(A):
-    pass
 
-class F(D, C, E):
-    pass
+class Horse(Animal):
 
-print(F.mro())
+    def __init__(self):
+        super().__init__()
 
-f = F()
-f.f()
+    def run(self):
+        print("i'm running!")
+
+
+class Eagle(Animal):
+
+    def __init__(self):
+        super().__init__()
+
+    def fly(self):
+        print("i'm flying!")
+
+
+class Pegasus(Horse, Eagle):
+
+    def __init__(self):
+        # В родительских классах тоже используется super(),
+        # поэтому все инициализаторы сработают в порядке MRO
+        super().__init__()
+
+
+class Rabbit(Horse, Eagle, metaclass=MyMRO):
+
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == '__main__':
+    pegasus = Pegasus()
+    pegasus.run()
+    pegasus.fly()
+    print(pegasus.weight)
+
+    # [<class '__main__.Pegasus'>, <class '__main__.Horse'>,
+    # <class '__main__.Eagle'>, <class '__main__.Animal'>, <class 'object'>]
+    print(Pegasus.mro())
+
+    rabbit = Rabbit()
+    # (<class '__main__.Rabbit'>, <class '__main__.Animal'>, <class 'object'>)
+    print(Rabbit.mro())
+    try:
+        rabbit.run()
+    except AttributeError as e:
+        print(e)
+
+"""
+Когда нельзя линеаризировать
+неразрешимые примеры:
+    class X: ...
+    class Y: ...
+    class A(X, Y): ...
+    class B(Y, X): ...
+    class G(A, B): ...
+Для A порядок X -> Y, а для B – обратный Y -> X
+
+class X: ...
+class Y(X): ...
+class A(X, Y): ...
+Здесь класс X наследуется дважды
+нарушает либо порядок старшинства либо порядка
+
+"""
